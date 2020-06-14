f1a262200bc4dfe875d7bd8d73ab3dd280731573
Arnie Serrano <arniekidd@gmail.com>
2020-06-14 03:47:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,10 +4,31 @@ var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 function destructivelyAppendKitten(name){
   kittens.push(name)
 }
+
 function destructivelyPrependKitten(name){
   kittens.unshift(name)
 }
 
 function destructivelyRemoveLastKitten(){
   kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+}
+
+function appendKitten(kittens,name){
+  return[...kittens, name]
+}
+
+function prependKitten(name){
+    return[name,...kittens]
+}
+
+function removeLastKitten(){
+  return kittens.slice(0, kittens.length-1)
+}
+
+function removeFirstKitten(){
+  return kittens.slice(1)
 }

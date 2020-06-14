c90f5872a3433fc16e3b39b3a3d2601c9a8c936e
Arnie Serrano <arniekidd@gmail.com>
2020-06-14 03:46:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,19 +12,3 @@ function destructivelyPrependKitten(name){
 function destructivelyRemoveLastKitten(){
   kittens.pop()
 }
-function destructivelyRemoveFirstKitten(){
-  kittens.shift()
-}
-function appendKitten(kittens,name){
-  return[...kittens, name]
-}
-
-function prependKitten(name){
-    return[name,...kittens]
-}
-function removeLastKitten(){
-  return kittens.slice(0, kittens.length-1)
-}
-function removeFirstKitten(){
-  return kittens.slice(1)
-}

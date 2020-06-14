f2297ad1602673c37f1c14f4cf5b2c2556067844
Arnie Serrano <arniekidd@gmail.com>
2020-06-14 03:09:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 function appendKittens(bob){
   kittens.push("bob")
 }
-appendKittens(bob
+appendKittens(bob)
 )
 function removeLastKitten(){
   kittens.unshift(bob)
